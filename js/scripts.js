9aed7d363c15a9bb0b5fb10a49d9c204be82d34e
Josh <Joshua.horgen@gmail.com>
2019-06-07 15:03:45 -0700

    add survey questions to index.html, comments. can't get js/jquery to work.

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,25 @@ $(document).ready(function(){
   console.log("jquery is working")
   $("#program").submit(function(event) {
     event.preventDefault();
-    var operator = $("input:checkbox[name=operator]:checked").val();
-    var A = $("#ses").val();
-    var B = parseInt($("#ses").val());
-    var C = parseInt($("#ses").val());
-    var D = parseInt($("#ses").val());
-    var E = parseInt($("#ses").val());
-    var F = parseInt($("#ses").val());
+
+    var A = parseInt($("#test").val());
+    var B = parseInt($("#test").val());
+    var C = parseInt($("#test").val());
+    var D = parseInt($("#test").val());
+    var E = parseInt($("#test").val());
+    var F = parseInt($("#test").val());
     var G = parseInt($("#G").val());
     var H = parseInt($("#H").val());
     var I = parseInt($("#I").val());
     var J = parseInt($("#J").val());
     var K = parseInt($("#K").val());
     var L = parseInt($("#L").val());
-    var result;
+    var operator = $("input:checkbox[name=operator]:checked").val();
+    // var result;
     console.log("hi");
 
     function result() {
-      if (ses === A) {
+      if (operator = ) {
         console.log("selected A")
         return ("Pytho")
       } else {
